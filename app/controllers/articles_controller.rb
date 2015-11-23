87770ca18d122867b9c401c8e6303b9b9390e1a2
Brittany See <brittanyjsee@gmail.com>
2015-11-23 03:29:21 +0000

    updating articles and implementing view partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    @article = Article.new(ariticle_params)
+    @article = Article.new(article_params)
  
     if @article.save
       redirect_to @article
@@ -21,8 +25,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
-    def ariticle_params
+    def article_params
       params.require(:article).permit(:title, :text)
     end
 end
